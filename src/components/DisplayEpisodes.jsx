45eb8f1e861effa50043881b35a99638a5b0c85a
Muhammad Tanvir Bin Ali <ali2912@outlook.com>
2021-11-09 00:30:51 +0000

    Level 100 complete

---
+++ src/components/DisplayEpisodes.jsx
@@ -0,0 +1,37 @@
+import episodes from "../data/episodes.json";
+
+export default function DisplayEpisodes() {
+  return (
+    <>
+      <h1>This is Level 100 of Tv Shows</h1>
+      <div className="list of episodes">
+        {episodes.map((episode) => (
+          <DisplaySingleEpisode key={episode.id} SingleEpisode={episode} />
+        ))}
+      </div>
+      <footer>
+        Credits: Episode data is originally from the fantastic{" "}
+        <a href="https://tvmaze.com/">TVmaze</a> via their{" "}
+        <a href="https://www.tvmaze.com/api">API</a>.
+      </footer>
+    </>
+  );
+}
+
+function DisplaySingleEpisode({ SingleEpisode }) {
+  return (
+    <div className="single-episode">
+      <h3>
+        {SingleEpisode.name} - {formatSeason(SingleEpisode)}
+      </h3>
+      <img src={SingleEpisode.image.medium} alt="single episode screengrab" />
+      <p>{SingleEpisode.summary}</p>
+    </div>
+  );
+}
+
+function formatSeason(SingleEpisode) {
+  return `S${SingleEpisode.season
+    .toString()
+    .padStart(2, "0")}E${SingleEpisode.number.toString().padStart(2, "0")}`;
+}
